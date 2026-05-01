a0a8fa6c55390aed5e1d98253007a70a8451ccc5
minotaa <jpichardo369@gmail.com>
2026-05-01 13:22:33 -0400

    hmmm
    Signed-off-by: minotaa <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/listeners/ChunkPopulateListener.kt
+++ src/main/kotlin/pink/mino/kraftwerk/listeners/ChunkPopulateListener.kt
@@ -1,32 +1,31 @@
 package pink.mino.kraftwerk.listeners
 
+import me.lucko.helper.Schedulers
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.world.ChunkPopulateEvent
-import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.scheduler.BukkitRunnable
-import pink.mino.kraftwerk.Kraftwerk
+import org.bukkit.event.world.ChunkLoadEvent
 import pink.mino.kraftwerk.events.ChunkModifiableEvent
 import pink.mino.kraftwerk.features.ConfigFeature
 
 class ChunkPopulateListener : Listener {
 
     @EventHandler
-    fun on(event: ChunkPopulateEvent) {
-        val worldName = event.world.name
+    fun on(event: ChunkLoadEvent) {
+        if (!event.isNewChunk) return
 
+        val worldName = event.world.name
         val chunkX = event.chunk.x
         val chunkZ = event.chunk.z
 
-        object : BukkitRunnable() {
-            override fun run() {
-                val world = Bukkit.getWorld(worldName) ?: return
-                val chunk = world.getChunkAt(chunkX, chunkZ) ?: return
+
+        Schedulers.async().runLater({
+            val world = Bukkit.getWorld(worldName) ?: return@runLater
+            world.getChunkAtAsync(chunkX, chunkZ).thenAcceptAsync { chunk ->
                 if (ConfigFeature.instance.worlds!!.getString("${world.name}.type") != null) {
                     Bukkit.getPluginManager().callEvent(ChunkModifiableEvent(chunk))
                 }
             }
-        }.runTaskLater(JavaPlugin.getPlugin(Kraftwerk::class.java), 400L)
+        }, 1L)
     }
 }
